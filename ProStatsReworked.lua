18d1bd4079fea14e8170a4de64c60ac7342ab12a
thelaw44 <ahmetcanguven44@gmail.com>
2015-07-06 15:42:07 +0300

    Update ProStatsReworked.lua

--- ProStatsReworked.lua
+++ ProStatsReworked.lua
@@ -1,27 +1,33 @@
 local proStatsVersion = 1.0
 
+
+class 'ProStats'
+	function ProStats:__init()
+		--self
+	end
+
 class 'Update'
 	function Update:__init(version)
-		--|> Script info
 		self.version     = version
 		self.scriptLink  = "https://raw.githubusercontent.com/thelaw44/prostats/master/ProStatsReworked.lua"
-		self.versionLink = "https://raw.githubusercontent.com/thelaw44/prostats/master/ProStatsReworked.version"
+		self.versionLink = "/thelaw44/prostats/master/ProStatsReworked.version".."?random="..math.random(1,10000)
 		self.path        = SCRIPT_PATH .. _ENV.FILE_NAME
-
-		--|> Variables
+		self.dlLib = false
+		self.dlSprite = false
 		self.needUpdate  = false
 		self.ranUpdater  = false
-
-		--|> Callbacks
+		self.commonReady = false
+		self.spriteReady = false
+		self.allReady = false
 		AddTickCallback(function () self:Tick() end)
 	end
 
 	function Update:Tick()
 		if not self.ranUpdater then
-			GetAsyncWebResult("raw.github.com" , self.versionLink, function(Data)
+			GetAsyncWebResult("raw.githubusercontent.com" , self.versionLink, function(Data)
  	           local onlineVersion = tonumber(Data)
  	           if onlineVersion and onlineVersion > proStatsVersion then
- 	           		print("<font color=\"#FF0000\">[Nintendo Katarina]:</font> <font color=\"#FFFFFF\">Found Update: </font> <font color=\"#FF0000\">"..proStatsVersion.." > "..onlineVersion.." Updating... Don't F9!!</font>")
+ 	           		print("<font color=\"#FF0000\">[Pro Stats]:</font> <font color=\"#FFFFFF\">Found Update: </font> <font color=\"#FF0000\">"..proStatsVersion.." > "..onlineVersion.." Updating... Don't F9!!</font>")
  	           		self.needUpdate = true
  	           end
  	        end)
@@ -30,14 +36,46 @@ class 'Update'
 		if self.needUpdate then
 			DownloadFile(self.scriptLink, self.path, function()
                 if FileExist(self.path) then
-                    print("<font color=\"#FF0000\">[Nintendo Katarina]:</font> <font color=\"#FFFFFF\">updated! Double F9 to use new version!</font>")
+                    print("<font color=\"#FF0000\">[Pro Stats]:</font> <font color=\"#FFFFFF\">updated! Double F9 to use new version!</font>")
                 end
             end)
             self.needUpdate = false
 		end
+
+		if not FileExist(LIB_PATH .. "/LawsUI.lua") then
+			if not self.dlLib then
+				self.dlLib = true
+				DownloadFile("https://raw.githubusercontent.com/thelaw44/LawsUI/master/common/LawsUI.lua", LIB_PATH .. "/LawsUI.lua", function()
+	                if FileExist(LIB_PATH .. "/LawsUI.lua") then
+	                    print("<font color=\"#FF0000\">[Pro Stats]:</font> <font color=\"#FFFFFF\">Laws UI downloaded.</font>")
+	                    self.dlLib = false
+	                end
+	            end)
+			end
+		else
+			self.commonReady = true
+		end
+
+		if not FileExist(SPRITE_PATH .. "/ProStats/stats-ui-empty.png") then
+			if not self.dlSprite then
+				self.dlSprite = true
+				DownloadFile("https://raw.githubusercontent.com/thelaw44/prostats/master/stats-ui-empty.png", SPRITE_PATH .. "/ProStats/stats-ui-empty.png", function()
+	                if FileExist(LIB_PATH .. "/LawsUI.lua") then
+	                    print("<font color=\"#FF0000\">[Pro Stats]:</font> <font color=\"#FFFFFF\">Sprites downloaded</font>")
+	                    self.dlSprite = false
+	                end
+	            end)
+			end
+		else
+			self.spriteReady = true
+		end
+
+		if self.spriteReady and self.commonReady and not self.needUpdate then
+			self.allReady = true
+		end
 	end
-end
 
 function OnLoad()
+	print("Loaded")
 	Update(proStatsVersion)
 end
